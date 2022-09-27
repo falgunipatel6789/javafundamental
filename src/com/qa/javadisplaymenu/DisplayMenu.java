ebcc18801a1d7b49133127d67ce5bc50efeb88fb
falgunipatel6789 <patelfalguni532@gmail.com>
2022-09-27 11:04:41 +0100

    java_fundamental_exrcise

---
+++ src/com/qa/javadisplaymenu/DisplayMenu.java
@@ -0,0 +1,16 @@
+package com.qa.javadisplaymenu;
+
+public class DisplayMenu {
+
+	public static void main(String[] args) {
+	
+         System.out.println("Hey !! Welcome to Online Shopping !! ");
+         System.out.println("Choose Your Option (1-4) ");
+         System.out.println("       1. Add Product\r\n"
+         		+ "	2. Product Details\r\n"
+         		+ "	3. Update Product\r\n"
+         		+ "	4. Delete Product\r\n"
+         		+ " ");
+	}
+
+}
